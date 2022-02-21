c928763acc558c076192461cdcbcfa6e40f6164f
Jonathan Emil Mogensen <jmog@itu.dk>
2022-02-21 10:47:34 +0100

    vec looks good on lvl 1

--- Stable_Baseline_Test.py
+++ Stable_Baseline_Test.py
@@ -118,8 +118,8 @@ env = DummyVecEnv([lambda: env1,lambda: env2,lambda: env3,lambda: env4,lambda: e
 
 
 #train(512,"saved_agents/vectorized/lvl_1/", env, 0.00005, 0)
-train(512,"saved_agents/steps/lvl_1/", env, 0.00005, 1000000)
-train(512,"saved_agents/steps/lvl_1/", env, 0.00005, 2000000)
+train(512,"saved_agents/vectorized/lvl_1/", env, 0.00005, 1000000)
+train(512,"saved_agents/vectorized/lvl_1/", env, 0.00005, 2000000)
 #train(512,"saved_agents/steps/lvl_1/", env, 0.00005, 3000000)
 #train(512,"saved_agents/steps/lvl_1/", env, 0.00005, 4000000)
 validate_agent(env1, "saved_agents/vectorized/lvl_1/", 100, "vec;single;5e-5;lvl-1")

560954bee48ceaf4415499bb0f9511529260d020
Jonathan Emil Mogensen <jmog@itu.dk>
2022-02-23 14:42:47 +0100

    Cleaned the last ones

--- Playground.py
+++ Playground.py
@@ -4,7 +4,7 @@ from os import listdir
 from os.path import isfile, join
 
 agent_val_path = os.path.dirname(os.path.realpath(__file__)) +"\\agent_validations\\"
-all_validations = [f for f in listdir(agent_val_path+"old_disc_vec\\") if isfile(join(agent_val_path+"old_disc_vec\\", f))]
+all_validations = [f for f in listdir(agent_val_path) if isfile(join(agent_val_path, f))]
 
 for val in all_validations:
-    Validate_Agents.sort_csv_file(agent_val_path+"old_disc_vec\\"+val)
+    Validate_Agents.sort_csv_file(agent_val_path+val)

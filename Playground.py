75ea07cd0fb66bed53b15dc791cce8efc7b43caa
Jonathan Emil Mogensen <jmog@itu.dk>
2022-04-18 12:58:22 +0200

    Ready to test 26 and 27

--- Playground.py
+++ Playground.py
@@ -36,7 +36,7 @@ def train(arl: ARLPCG, hours_run):
 
 level_folder ="MAFGym/levels/original/subset/completable/"
 #--------------------------------------------------------------------------------------------
-arl_save_folder = "saved_arl/14_2/"
+arl_save_folder = "saved_arl/26/"
 generated_level_path = os.path.dirname(os.path.realpath(__file__)).replace("\\MAFGym", "") + "\\generated_levels\\"
 arl=None
 arl = ARLPCG(
@@ -47,19 +47,29 @@ arl = ARLPCG(
     internal=1, 
     external=1,
     gen_steps=32, 
-    aux_switch=10,
+    aux_switch=1,
     pcg_env_type=PCGEnvType.SIM)
 
 train(arl, 12)
 
-validate_arl(arl, 100, 10, "14_2_arl")
+validate_arl(arl, 100, 10, "26_arl")
+
+arl_save_folder = "saved_arl/27/"
+arl=None
+arl = ARLPCG(
+    load_path="", 
+    levels_path=level_folder, 
+    generate_path=generated_level_path, 
+    save_name="arl-dev", 
+    internal=1, 
+    external=1,
+    gen_steps=32, 
+    aux_switch=100,
+    pcg_env_type=PCGEnvType.SIM)
 
 train(arl, 12)
 
-validate_arl(arl, 100, 10, "14_2_arl_24")
+validate_arl(arl, 100, 10, "27arl")
 
-#train(arl, 6)
-
-#validate_arl(arl, 100, 10, "23_arl_30")
 
 
